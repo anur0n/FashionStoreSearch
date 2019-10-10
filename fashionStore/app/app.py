c377c5b0c5a34f77ae00e0e0ea5a0ef368606102
Md Ashaduzzaman Rubel Mondol <ashaduzzamanrubel@gmail.com>
2019-10-10 01:17:59 +0000

    Pushing first commit with all files from pythonAnywhere

---
+++ fashionStore/app/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request
+import time
+from dmrankedquery import QueryType, QueryHandler
+#from tqdm import tqdm
+
+app = Flask(__name__)
+
+queryHandler = QueryHandler()
+
+isIndexLoaded = False
+
+def highlight_term(term, text):
+    replaced_text = text.replace(term, "<mark>{term}</mark>".format(term=term))
+    return replaced_text
+
+IMAGES_PATH = 'static/images/'
+@app.route('/', methods=['GET', 'POST'])
+def search():
+    # if 'query' in request.args:
+    #     q = request.args.get("q")
+    #     query = q.split()
+    #     # communicate with sub process
+    #     return render_template('search/index.html', q=q, query=query)
+
+    if request.method == 'POST':
+        global isIndexLoaded
+        if not isIndexLoaded:
+            queryHandler.prepareParams()
+            queryHandler.readIndex()
+            isIndexLoaded = True
+            print('Index ..' + str(len(queryHandler.index)))
+
+        query = request.form['query']
+        print('searching ..' + query)
+        docs, scores = queryHandler.performQuery(query)
+
+        titles = [x.displayName for x in docs]
+        images = [IMAGES_PATH+x.image+'.jpg' for x in docs]
+        descriptions = [x.description for x in docs]
+
+        for j, term in enumerate(queryHandler.getTerms(query)):
+            for i, title in enumerate(titles):
+                titles[i] = highlight_term(term, title)
+            for i, description in enumerate(descriptions):
+                descriptions[i] = highlight_term(term, description)
+        outStr = ''
+        return render_template('search/index.html', titles = titles, images = images, descriptions = descriptions, scores=scores, query = query)
+
+    #     for i, doc in enumerate(docs):
+    #         outStr += str(i+1)+'<br> <b>'+doc.displayName + '</b><br> <br>' + doc.description + '<br> <br> <br>'
+    #     for j, term in enumerate(queryHandler.getTerms(query)):
+    #             highlight_term(term, outStr)
+    #     print('search complete')
+    #     return outStr
+    return render_template('search/index.html') #render_template("index.html")
+
+
+#def index():
+ #   return str(queryHandler.performQuery(' toy'))
+
+if __name__ == '__main__':
+    queryHandler.prepareParams()
+    queryHandler.readIndex()
+    queryHandler.performQuery('Blue check shirt')
+    print('Search complete')
+
+    #app.run()
---
+++ fashionStore/app/docRetriever.py
@@ -0,0 +1,57 @@
+import csv
+#from tqdm import tqdm
+
+MY_DRIVE = ''#'/content/drive/My Drive/Data Mining'
+
+STYLE_WITH_DESC_N_TITLE = MY_DRIVE+'/home/anur0nArm/fashionStore/app/styles_with_description_title.csv'
+
+STYLE_WITH_DESC_N_TITLE_RANDOM = MY_DRIVE+'/home/anur0nArm/fashionStore/app/styles_with_description_title_RandomSampled.csv'
+
+INVERTED_IDX_FILE = MY_DRIVE+'/home/anur0nArm/fashionStore/app/store_index.dat'
+
+DOC_ID_COL_IDX = 0
+
+DISPLAY_NAME_COL_IDX = 9
+
+DESCR_COL_IDX = 18
+
+
+class ProductDoc():
+    def __init__(self):
+        self.id = None
+        self.image = None
+        self.displayName = None
+        self.description = None
+
+class DocRetriever():
+    def __init__(self):
+        pass
+
+    def retrieveDocs(self, docIds):
+        docs = []
+
+        docIds=[str(x) for x in docIds]
+
+        with open(STYLE_WITH_DESC_N_TITLE_RANDOM, 'r', encoding='latin-1') as csvfile:
+            reader = csv.reader(csvfile)
+
+            #print(docIds)
+
+            for rowNo, row in enumerate(reader):
+              #  if rowNo == 0:
+               #     print(row[0])
+                #print('\t ' + str(row[DOC_ID_COL_IDX]))
+                if row[DOC_ID_COL_IDX] in docIds:
+                    doc = ProductDoc()
+                    doc.id = row[DOC_ID_COL_IDX]
+                    doc.image = row[DOC_ID_COL_IDX]
+                    doc.displayName = row[DISPLAY_NAME_COL_IDX]
+                    doc.description = row[DESCR_COL_IDX]
+                    doc.displayName = doc.displayName.replace('`', ',')
+                    doc.description = doc.description.replace('`', ',')
+                    docs.append(doc)
+                    #break
+        return docs
+
+
+
